a00b31b6ab367b7eaacb119154700144faedb01f
tylervila1 <86322241+tylervila1@users.noreply.github.com>
2022-10-04 14:33:28 -0600

    Add files via upload

---
+++ 05_machine-setup/05_02_yeehaw.py
@@ -0,0 +1,10 @@
+# In this script, write code that displays the follow message:
+#
+# 	I'm a programmer now.
+# 	Yeehaw!
+# 	Coding here I come!
+
+
+
+
+print("I'm a programmer now. Yeehaw! Coding here i come!")
---
+++ 05_machine-setup/05_04_seconds_year.py
@@ -0,0 +1,3 @@
+# Use the interpreter to calculate how many seconds are in a year.
+# Then write the code in this script file below this line.
+60*24*7*4*12
